f97fccf0d696fe9f91566222276c68d93c1a6ebd
cicithv <cicithv@gmail.com>
2024-03-27 11:22:20 +0700

    tugas 1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Cici
+            Saya tinggaL di Paris 2
+            kegiatan yang saya sukai adalah
+            mendengar lagu K-pop";
+    }
+}
